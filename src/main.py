3157d24b81a884c685e1a52eaac936028e76d1db
SungMinCho <tjdals4565@gmail.com>
2016-05-08 03:27:42 +0900

    Add comma

--- src/main.py
+++ src/main.py
@@ -467,7 +467,7 @@ def snupl_test():
   p52_3 = Production(SUBROUTINEDECLS, String([SUBROUTINEDECLS, SUBROUTINEDECL]))
   p52 = Production(MODULE, String([TERMINALMODULE, IDENT, SEMICOLON, VARDECLARATION, SUBROUTINEDECLS, BEGIN, STATSEQUENCE, END, IDENT, DOT]))
 
-  ps = [pt1,pt2,pt3,p1,p2,p3,p4,p5,p6,p7,p8,p9,p10,p11,p12,p13,p14,p15,p16,p17,p18,p19,p20,p21,p22,p23,p24_1,p24_2,p24,p25,p26,p27,p28,p29,p30,p31,p32,p33 \
+  ps = [pt1,pt2,pt3,p1,p2,p3,p4,p5,p6,p7,p8,p9,p10,p11,p12,p13,p14,p15,p16,p17,p18,p19,p20,p21,p22,p23,p24_1,p24_2,p24,p25,p26,p27,p28,p29,p30,p31,p32,p33,\
         p34,p35,p36,p37,p38,p39,p40,p41,p42_1,p42_2,p42,p43,p44,p45,p46,p47,p48,p49,p50,p51,p52_1,p52_2,p52_3,p52]
 
   draw_CanonicalLR_0(MODULE, ps)
